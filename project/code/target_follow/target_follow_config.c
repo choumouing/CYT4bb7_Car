cf3e43e3ef36a59888a692bdd8dd87f30c690218
msj <2305514816@qq.com>
2026-05-16 01:10:21 +0800

    uwb测试

--- project/code/target_follow/target_follow_config.c
+++ project/code/target_follow/target_follow_config.c
@@ -6,9 +6,7 @@ void target_follow_load_default_targets(void)
 {
     target_follow_clear_targets();
 
-    (void)target_follow_add_target(2.00f, 2.00f);
-    (void)target_follow_add_target(-2.00f, -0.50f);
-    (void)target_follow_add_target(0.00f, -1.50f);
+    (void)target_follow_add_target(50.00f, 50.00f);
 
     target_follow_restart_targets();
 }
