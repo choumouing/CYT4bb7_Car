daffe1ad202f4394b1eb196e96a0acb308bfaeb6
ZhangStudyLife <174326754+ZhangStudyLife@users.noreply.github.com>
2026-06-20 08:48:42 +0800

    调节yaw参数了

--- project/code/Controller/car_mode4.c
+++ project/code/Controller/car_mode4.c
@@ -4,12 +4,51 @@
 #define MODE4_HOLD_MS           (2000U)
 #define MODE4_STEP_MS           (1000U)
 #define MODE4_FULL_TURN_DEG     (360.0f)
-#define MODE4_RATE_COUNT        ((uint32)(sizeof(s_mode4_rates_deg_s) / sizeof(s_mode4_rates_deg_s[0])))
+#define MODE4_STEP_RATE_COUNT   ((uint32)(sizeof(s_mode4_step_rates_deg_s) / sizeof(s_mode4_step_rates_deg_s[0])))
+#define MODE4_SMOOTH_RATE_COUNT ((uint32)(sizeof(s_mode4_smooth_rates_deg_s) / sizeof(s_mode4_smooth_rates_deg_s[0])))
 
-static const float s_mode4_rates_deg_s[] = {30.0f, 60.0f, 90.0f};
+static const float s_mode4_step_rates_deg_s[] = {30.0f, 90.0f};
+static const float s_mode4_smooth_rates_deg_s[] = {10.0f, 45.0f};
 static uint8 s_mode4_started = 0U;
 static uint32 s_mode4_start_ms = 0U;
 static float s_mode4_yaw_target_deg = 0.0f;
+
+static uint32 car_mode4_step_phase_ms(float rate_deg_s)
+{
+    return (uint32)((MODE4_FULL_TURN_DEG / rate_deg_s) * 2.0f) * MODE4_STEP_MS;
+}
+
+static uint32 car_mode4_smooth_phase_ms(float rate_deg_s)
+{
+    return (uint32)((MODE4_FULL_TURN_DEG * 2.0f * 1000.0f) / rate_deg_s);
+}
+
+static float car_mode4_step_target(float rate_deg_s, uint32 phase_ms)
+{
+    uint32 step_index = phase_ms / MODE4_STEP_MS;
+    uint32 steps_per_turn = (uint32)(MODE4_FULL_TURN_DEG / rate_deg_s);
+
+    if(step_index < steps_per_turn)
+    {
+        return rate_deg_s * (float)(step_index + 1U);
+    }
+
+    return MODE4_FULL_TURN_DEG -
+           rate_deg_s * (float)(step_index - steps_per_turn + 1U);
+}
+
+static float car_mode4_smooth_target(float rate_deg_s, uint32 phase_ms)
+{
+    float phase_s = (float)phase_ms * 0.001f;
+    float one_turn_s = MODE4_FULL_TURN_DEG / rate_deg_s;
+
+    if(phase_s < one_turn_s)
+    {
+        return rate_deg_s * phase_s;
+    }
+
+    return MODE4_FULL_TURN_DEG - rate_deg_s * (phase_s - one_turn_s);
+}
 
 void car_mode4_init(void)
 {
@@ -28,14 +67,9 @@ void car_mode4_update_100HZ(uint32 now_ms)
     uint32 elapsed_ms;
     uint32 active_ms;
     uint32 cycle_ms;
-    uint32 rate_index;
-    uint32 rate_phase_ms;
-    uint32 step_index;
-    uint32 steps_per_turn;
-    uint32 steps_per_rate;
+    uint32 phase_ms;
+    uint32 span_ms;
     uint32 i;
-    float rate_deg_s;
-    float target_deg;
 
     if(0U == s_mode4_started)
     {
@@ -52,41 +86,43 @@ void car_mode4_update_100HZ(uint32 now_ms)
     {
         active_ms = elapsed_ms - MODE4_HOLD_MS;
         cycle_ms = 0U;
-        for(i = 0U; i < MODE4_RATE_COUNT; i++)
+        for(i = 0U; i < MODE4_STEP_RATE_COUNT; i++)
         {
-            cycle_ms += (uint32)((MODE4_FULL_TURN_DEG / s_mode4_rates_deg_s[i]) * 2.0f) *
-                        MODE4_STEP_MS;
+            cycle_ms += car_mode4_step_phase_ms(s_mode4_step_rates_deg_s[i]);
+        }
+        for(i = 0U; i < MODE4_SMOOTH_RATE_COUNT; i++)
+        {
+            cycle_ms += car_mode4_smooth_phase_ms(s_mode4_smooth_rates_deg_s[i]);
         }
 
         active_ms %= cycle_ms;
-        rate_phase_ms = active_ms;
-        rate_index = 0U;
-        for(i = 0U; i < MODE4_RATE_COUNT; i++)
+        phase_ms = active_ms;
+        for(i = 0U; i < MODE4_STEP_RATE_COUNT; i++)
         {
-            steps_per_rate = (uint32)((MODE4_FULL_TURN_DEG / s_mode4_rates_deg_s[i]) * 2.0f);
-            if(rate_phase_ms < (steps_per_rate * MODE4_STEP_MS))
+            span_ms = car_mode4_step_phase_ms(s_mode4_step_rates_deg_s[i]);
+            if(phase_ms < span_ms)
             {
-                rate_index = i;
-                break;
+                s_mode4_yaw_target_deg = car_mode4_step_target(s_mode4_step_rates_deg_s[i],
+                                                               phase_ms);
+                car_forward_target = 0.0f;
+                car_strafe_target = 0.0f;
+                return;
             }
-            rate_phase_ms -= steps_per_rate * MODE4_STEP_MS;
+            phase_ms -= span_ms;
         }
-
-        rate_deg_s = s_mode4_rates_deg_s[rate_index];
-        steps_per_turn = (uint32)(MODE4_FULL_TURN_DEG / rate_deg_s);
-        step_index = rate_phase_ms / MODE4_STEP_MS;
-
-        if(step_index < steps_per_turn)
+        for(i = 0U; i < MODE4_SMOOTH_RATE_COUNT; i++)
         {
-            target_deg = rate_deg_s * (float)(step_index + 1U);
+            span_ms = car_mode4_smooth_phase_ms(s_mode4_smooth_rates_deg_s[i]);
+            if(phase_ms < span_ms)
+            {
+                s_mode4_yaw_target_deg = car_mode4_smooth_target(s_mode4_smooth_rates_deg_s[i],
+                                                                 phase_ms);
+                car_forward_target = 0.0f;
+                car_strafe_target = 0.0f;
+                return;
+            }
+            phase_ms -= span_ms;
         }
-        else
-        {
-            target_deg = MODE4_FULL_TURN_DEG -
-                         rate_deg_s * (float)(step_index - steps_per_turn + 1U);
-        }
-
-        s_mode4_yaw_target_deg = target_deg;
     }
 
     car_forward_target = 0.0f;
